e0487c3f38e46cb8b6bb63651cc0fd87db4fcea9
Laevatein0810 <71696488+Laevatein0810@users.noreply.github.com>
2020-10-18 18:59:10 +0900

    詳細・編集・削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,7 +3,27 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.all
   end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
   def show
+    @prototype = Prototype.find(params[:id])
   end
 
   def new
