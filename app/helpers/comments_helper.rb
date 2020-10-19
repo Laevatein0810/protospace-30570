80e3bceb2357bfb884a900b584e74a8691840e55
Laevatein0810 <71696488+Laevatein0810@users.noreply.github.com>
2020-10-19 12:54:12 +0900

    修正３

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,2 @@
-module CommnetsHelper
+module CommentsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :edit, :create, :destroy, :update, :show] do
-    resources :coments, only: :create
+    resources :comments, only: :create
   end
   resources :users, only: :show
 end
